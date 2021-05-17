fcdf9e376c7d7a6e38f6de496d472b4036aceb2b
Henry Leto <hnr.leto11@gmail.com>
2021-05-16 20:42:55 -0400

    messing with angular loops

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { Tile } from "../Models/Tile";
 import { TileComponent } from "../tile/tile.component";
@@ -8,16 +8,38 @@ import { TileComponent } from "../tile/tile.component";
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.scss']
 })
-export class GridComponent implements OnInit {
+export class GridComponent implements OnInit, AfterViewInit {
   Arr: number[] = [];
   Tiles: Tile[] = [];
+  HeightxWidth: Object = {
+    height: screen.height,
+    widyth: screen.width
+  }
+
+  @ViewChild('tile') elementView: ElementRef | undefined;
+
+  ScreenWidth: number = screen.width;
+
   constructor() { }
 
   ngOnInit(){
-    let digit = 100
+    this.generateArray()
+  }
+
+  generateArray() {
+    let digit = 71
 
     for (let i = 1;i < digit; i++){
       this.Arr.push(i)
     }
+  }
+
+  ngAfterViewInit() {
+    let divToMeasure = this.elementView?.nativeElement.offSetWdith;
+    console.log(divToMeasure);
+  }
+
+  calculateNumberOfTiles(){
+    this.ScreenWidth
   }
 }
