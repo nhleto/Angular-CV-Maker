0eaf2e8beaf80201c0643f927d100f3b44b8265f
Henry Leto <hnr.leto11@gmail.com>
2021-05-23 13:25:56 -0400

    add window service

---
+++ src/app/Services/window.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {fromEvent, Observable, Subscription} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WindowService {
+  resizeObservable$!: Observable<Event>
+
+  width!: number;
+  height!: number;
+
+  constructor() { }
+
+  subPub(){
+    this.resizeObservable$ = fromEvent(window, 'resize')
+    // this.resizeSubscription$ = this.resizeObservable$.subscribe( evt => {
+    //   this.test(evt)
+    // })
+  }
+
+  test = (evt: any) => {
+    this.width = evt.target.innerWidth
+    this.height = evt.target.innerHeight
+  }
+  //
+  // unSub(){
+  //   this.resizeSubscription$.unsubscribe()
+  // }
+
+}
--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,43 +1,49 @@
-import {Component, ElementRef, OnInit, ViewChild, AfterViewInit, OnDestroy} from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  AfterViewInit,
+  OnDestroy,
+  OnChanges,
+  SimpleChanges, Input
+} from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { ITile } from "../Models/ITile";
 import { TileComponent } from "../tile/tile.component";
 import {from, fromEvent, merge, Observable, of, Subscription} from "rxjs";
 import {element} from "protractor";
 import {distinctUntilChanged, map} from "rxjs/operators";
+import {WindowService} from "../Services/window.service";
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.scss']
 })
-export class GridComponent implements OnInit, OnDestroy {
+export class GridComponent implements OnInit, OnDestroy, OnChanges {
+  @Input() height = this.windowResize.height
+  @Input() width = this.windowResize.width
+
   Arr: number[] = [...Array(51).keys()];
   Tiles: ITile[] = [];
-  resizeObservable$!: Observable<Event>
   resizeSubscription$!: Subscription
-  width: any;
-  height: any;
 
-  @ViewChild('tile') tile: any;
-
-  constructor() { }
+  constructor(private windowResize: WindowService) { }
 
   ngOnInit(){
-    this.resizeObservable$ = fromEvent(window, 'resize')
-    this.resizeSubscription$ = this.resizeObservable$.subscribe( evt => {
-      this.test(evt)
-    })
+    this.windowResize.subPub()
   }
 
-  test = (evt: any) => {
-    this.width = evt.target.innerWidth
-    this.height = evt.target.innerHeight
-    console.log(this.width, this.height)
+  ngOnChanges(changes: SimpleChanges) {
+    this.height = this.windowResize.height
+    this.width = this.windowResize.width
+    console.log(this.height, this.width)
+    console.log(changes)
   }
 
   ngOnDestroy() {
-    this.resizeSubscription$.unsubscribe()
+    // this.windowResize.unSub()
   }
 
 }
